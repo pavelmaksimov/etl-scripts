b5fbf91cd2d59349655695763508085f2d28588c
pmaksimov <vur21@ya.ru>
2022-04-12 21:53:34 +0300

    add etl for ad stats

--- project/marylin-clickhouse.py
+++ project/marylin-clickhouse.py
@@ -14,6 +14,7 @@ from utils.transform_funcs import to_date, delete_row_on_error, to_decimal
 
 PROJECT_PLACEMENTS_TABLE_NAME = "mary_placements"
 STATS_TABLE_NAME = "mary_stats"
+AD_STATS_TABLE_NAME = "mary_ad_stats"
 
 LOGGING_FORMAT = "%(asctime)s [%(levelname)s] %(pathname)s:%(funcName)s  %(message)s"
 LOGGING_FILEPATH = pathlib.Path.cwd() / "etl-mary-clickhouse.log"
@@ -136,6 +137,130 @@ async def etl_stats(
         logger.warning("Number of not inserted rows %s lines", number_of_not_inserted_rows)
 
 
+async def etl_ad_stats(
+    mary_client: AsyncClient,
+    db_client: Client,
+    project_id: int,
+    db_name: str,
+    start_date: date,
+    end_date: date,
+):
+    db_table = f"{db_name}.{AD_STATS_TABLE_NAME}"
+
+    body = {
+        "channel_id": [],
+        "start_date": start_date.strftime("%Y-%m-%d"),
+        "end_date": end_date.strftime("%Y-%m-%d"),
+        "date_grouping": "day",
+        "grouping": "ad",
+        "filtering": [{"entity": "project", "entities": [project_id]}],
+        "custom_metrics": [],
+        "profiles": [],
+        "goals": [],
+        "with_vat": False,
+        "per_page": 200,
+        "sorting": "date",
+        "columns": [
+            "date",
+            "placement_id",
+            "campaign_xid",
+            "placement_name",
+            "adgroup_id",
+            "adgroup_name",
+            "ad_id",
+            "ad_name",
+            "ad_head",
+            "ad_head2",
+            "ad_head3",
+            "ad_text",
+            "ad_text2",
+            "ad_url",
+            "ad_format",
+            "impressions",
+            "clicks",
+            "video_views_start",
+            "video_views_2_sec",
+            "video_views_3_sec",
+            "video_views_6_sec",
+            "video_views",
+            "video_view_rate",
+            "engagements",
+            "engagement_rate",
+            "ctr",
+            "cost",
+            "cpc",
+            "cpm",
+            "cpv",
+            "cpe",
+            "cost_fact",
+            "cpc_fact",
+            "cpm_fact",
+            "cpv_fact",
+            "cpe_fact",
+            "link_clicks",
+            "group_clicks",
+            "post_likes",
+            "post_shares",
+            "follows",
+            "comments",
+            "join_rate",
+            "hides",
+            "reports",
+            "unsubscribes",
+            "leads",
+            "reach",
+            "reach_total",
+            "reach_total_sum",
+            "viral_reach_total_sum",
+            "reach_subscribers",
+            "revenue",
+            "revenue_model_orders",
+            "orders",
+            "model_orders",
+            "channel_id",
+        ],
+    }
+
+    logger.info("Create table '%s'", db_table)
+    db_client.execute(helper.create_table_of_mary_ad_stats_query.format(db_table))
+
+    logger.info("Drop partition in '%s'", db_table)
+    dates = iter_range_datetime(start_date, end_date, timedelta(days=1))
+    db_client.drop_partitions_by_dates(db_table, dates)
+
+    logger.info("Export ad stats")
+    async for page in mary_client.iter_statistics_detailed(body):
+        number_of_not_inserted_rows = 0
+        for i, item in enumerate(page["items"]):
+            count_page_rows = len(page["items"])
+
+            # Transform data types.
+            with delete_row_on_error(page["items"], i, logger) as row:
+                row["cost_fact"] = to_decimal(row["cost_fact"])
+                row["cpc_fact"] = to_decimal(row["cpc_fact"])
+                row["cpm_fact"] = to_decimal(row["cpm_fact"])
+                row["cpv_fact"] = to_decimal(row["cpv_fact"])
+                row["cpe_fact"] = to_decimal(row["cpe_fact"])
+                row["cost"] = to_decimal(row["cost"])
+                row["cpc"] = to_decimal(row["cpc"])
+                row["ctr"] = to_decimal(row["ctr"])
+                row["cpm"] = to_decimal(row["cpm"])
+                row["cpe"] = to_decimal(row["cpe"])
+                row["cpv"] = to_decimal(row["cpv"])
+                row["revenue"] = to_decimal(row["revenue"])
+                row["revenue_model_orders"] = to_decimal(row["revenue_model_orders"])
+                row["video_view_rate"] = to_decimal(row["video_view_rate"])
+                row["engagement_rate"] = to_decimal(row["engagement_rate"])
+                row["date"] = to_date(row["date"])
+
+            number_of_not_inserted_rows += count_page_rows - len(page["items"])
+
+        logger.info("Insert data to '%s'", db_table)
+        db_client.insert(db_table, page["items"])
+        logger.info("Inserted %s lines", len(page["items"]))
+        logger.warning("Number of not inserted rows %s lines", number_of_not_inserted_rows)
+
+
 async def main(
     api_root: str,
     token: str,
@@ -162,6 +287,7 @@ async def main(
 
     await etl_project_placements(mary_client, db_client, project_id, db_name)
     await etl_stats(mary_client, db_client, project_id, db_name, start_date, end_date)
+    await etl_ad_stats(mary_client, db_client, project_id, db_name, start_date, end_date)
 
     logger.info("Success")
 
